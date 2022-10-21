7ad714b1258252a864cb1783accf27cc811f6139
sebaraba <rabasebastians@gmail.com>
2022-10-21 22:13:28 +0300

    feat(test): add support for console debugging

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.9;
 
-
+import "hardhat/console.sol";
 // This is the main building block for smart contracts.
 contract Token {
     // Some string type variables to identify the token.
@@ -45,7 +45,14 @@ contract Token {
         // If `require`'s first argument evaluates to `false` then the
         // transaction will revert.
         require(balances[msg.sender] >= amount, "Not enough tokens");
-
+        
+        console.log(
+            "Transferring from %s to %s %s tokens",
+            msg.sender,
+            to,
+            amount
+        );
+        
         // Transfer the amount.
         balances[msg.sender] -= amount;
         balances[to] += amount;
