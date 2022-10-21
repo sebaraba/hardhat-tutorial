5ba1bbc44621f4ba8d0b33b83bbae9cc2a096ee1
sebaraba <rabasebastians@gmail.com>
2022-10-21 22:40:41 +0300

    feat(deploy): add goerli deploy config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
-};
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY]
+    }
+}
 
 export default config;
