e4c01a443671a615cff9e626bf22a4d43bcf6b63
RabaS <sebastian-stefan.raba@haufe.com>
2022-10-21 20:04:46 +0300

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+};
+
+export default config;
